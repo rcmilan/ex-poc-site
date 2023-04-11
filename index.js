cd6b44f367df3f99e681e0df58030f73f3cd264b
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-11 09:28:44 -0300

    icons

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
   listItems.forEach((item) => {
     item.addEventListener("mouseover", () => {
       const iconName = item.dataset.icon;
+
+      if (iconName === undefined) return;
+
       iconContainer.innerHTML = `<i class="fab fa-${iconName} fa-4x"></i>`;
       iconContainer.classList.add("active");
+
+      // Check if the screen width is 768px or less
+      if (window.innerWidth <= 768) {
+        // Create a new icon-container-small element and add it to the list item
+        const iconContainerSmall = document.createElement("div");
+        iconContainerSmall.classList.add("icon-container-small");
+        iconContainerSmall.innerHTML = `<i class="fab fa-${iconName}"></i>`;
+        item.appendChild(iconContainerSmall);
+      }
     });
 
     item.addEventListener("mouseout", () => {
       iconContainer.classList.remove("active");
+
+      // Remove the icon-container-small element when mouse leaves the list item
+      const iconContainerSmall = item.querySelector(".icon-container-small");
+      if (iconContainerSmall) {
+        item.removeChild(iconContainerSmall);
+      }
     });
   });
 });
